1732f0313ec8e6cc51eaff2525625c84ec69d71a
Pamela Ocampo <pamela.ocampo@gmail.com>
2014-08-14 23:12:33 -0700

    Using Yo generator for foundation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,181 @@
+'use strict';
+
 module.exports = function(grunt) {
 
-    // All configuration goes here
-    grunt.initConfig({
-    	pkg: grunt.file.readJSON('package.json'),
+    require('load-grunt-tasks')(grunt);
 
-    	jekyll: {
-    		build : {
-    			dest: '_site'
-    		}
-    	},
 
-    	sass: {
-    		dist: {
-    			files: {
-    				'assets/css/screen.css': 'assets/scss/screen.scss'
-    			}
-    		}
-    	},
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		app: '',
+		dist: 'dist',
 
-    	watch: {
-    		sass: {
-    			files: 'assets/scss/**/*.scss',
-    			tasks: ['sass']
-    		},
-    		jekyll: {
-    			files: [
-    			'**',
-    			'_layouts/*.html',
-    			'_includes/*.md',
-    			'assets/css/screen.css',
-    			'!_site/**',
-    			'!node_modules/**',
-    			'!.sass-cache/**',
-    			'!Gruntfile.js',
-    			'!package.json',
-    			'!.git/**'
-    			],
-    			tasks: ['jekyll']
-    		}
-    	}
-    });
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded', // expanded or nested or compact or compressed
+					loadPath: 'bower_components/foundation/scss',
+					compass: true,
+					quiet: true
+				},
+				files: {
+					'css/app.css': 'scss/app.scss'
+				}
+			}
+		},
 
-    // Load the plugins
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-jekyll');
+		
+		jade: {
+			compile: {
+				options: {
+					pretty: true,
+					data: {
+						debug: false
+					}
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= app %>/',
+					src: ['**/*.jade', '!**/header.jade', '!**/footer.jade'],
+					ext: '.html',
+					dest: '<%= app %>/'
+				}]
+			}
+		},
 
-    // Custom tasks
-    grunt.registerTask('build', ['sass', 'jekyll']);
-    grunt.registerTask('default', ['build', 'watch']);
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= app %>/js/**/*.js'
+			]
+		},
+
+		clean: {
+			dist: {
+				src: ['<%= dist %>/*']
+			},
+		},
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd:'<%= app %>/',
+					src: ['fonts/**', '**/*.html', '!**/*.scss', '!bower_components/**', '!talks/**'],
+					dest: '<%= dist %>/'
+				} , {
+					expand: true,
+					flatten: true,
+					src: ['<%= app %>/bower_components/font-awesome/fonts/**'],
+					dest: '<%= dist %>/fonts/',
+					filter: 'isFile'
+				} ]
+			},
+		},
+
+		imagemin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: '<%= app %>/images/',
+					src: ['**/*.{jpg,gif,svg,jpeg,png}'],
+					dest: '<%= dist %>/images/'
+				}]
+			}
+		},
+
+		uglify: {
+			options: {
+				preserveComments: 'some',
+				mangle: false
+			}
+		},
+
+		useminPrepare: {
+			html: ['<%= app %>/index.html'],
+			options: {
+				dest: '<%= dist %>'
+			}
+		},
+
+		usemin: {
+			html: ['<%= dist %>/**/*.html', '!<%= app %>/bower_components/**'],
+			css: ['<%= dist %>/css/**/*.css'],
+			options: {
+				dirs: ['<%= dist %>']
+			}
+		},
+
+		watch: {
+			grunt: {
+				files: ['Gruntfile.js'],
+				tasks: ['sass']
+			},
+			sass: {
+				files: 'scss/**/*.scss',
+				tasks: ['sass']
+			},
+			jade: {
+				files: ['index.jade', '**/*.jade'],
+				tasks: ['jade']
+			},
+			livereload: {
+				files: ['**/*.html', '!talks/**', '!bower_components/**', 'js/**/*.js', 'css/**/*.css', 'images/**/*.{jpg,gif,svg,jpeg,png}'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+
+		connect: {
+			app: {
+				options: {
+					port: 9000,
+					base: '',
+					open: true,
+					livereload: true,
+					hostname: '127.0.0.1'
+				}
+			},
+			dist: {
+				options: {
+					port: 9001,
+					base: '<%= dist %>/',
+					open: true,
+					keepalive: true,
+					livereload: false,
+					hostname: '127.0.0.1'
+				}
+			}
+		},
+
+		wiredep: {
+			target: {
+				src: [
+					'**/*.jade'
+				],
+				exclude: [
+					'modernizr',
+					'font-awesome',
+					'jquery-placeholder',
+					'jquery.cookie',
+					'foundation'
+				]
+			}
+		}
+
+	});
+
+	grunt.registerTask('compile-jade', ['jade']);
+	grunt.registerTask('compile-sass', ['sass']);
+	grunt.registerTask('bower-install', ['wiredep']);
+	
+	grunt.registerTask('default', ['compile-jade', 'compile-sass', 'bower-install', 'connect:app', 'watch']);
+	grunt.registerTask('validate-js', ['jshint']);
+	grunt.registerTask('server-dist', ['connect:dist']);
+	
+	grunt.registerTask('publish', ['compile-jade', 'compile-sass', 'clean:dist', 'validate-js', 'useminPrepare', 'copy:dist', 'newer:imagemin', 'concat', 'cssmin', 'uglify', 'usemin']);
+
 };
