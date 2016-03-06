1dbdc5721d07d76e2b161ff6bd68f2a62f8d43ff
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-03-05 22:11:58 -0800

    read the next post in the list of pages if one is not defined

--- components/ReadNext.js
+++ components/ReadNext.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router'
 import { link } from 'gatsby-helpers'
 import { prune, include as includes } from 'underscore.string'
-import find from 'lodash/find'
+import { find, findIndex } from 'lodash'
 import { rhythm, fontSizeToMS } from 'utils/typography'
 
 class ReadNext extends React.Component {
@@ -16,48 +16,53 @@ class ReadNext extends React.Component {
       )
     }
     if (!nextPost) {
-      return React.createElement('noscript', null)
-    } else {
-      nextPost = find(pages, (page) =>
-        includes(page.path, readNext.slice(1, -1))
+      const currentIndex = findIndex(pages, (page) =>
+        includes(page.path, post.path)
       )
-      // Create pruned version of the body.
-      const html = nextPost.data.body
-      const body = prune(html.replace(/<[^>]*>/g, ''), 200)
+      nextPost = pages[(currentIndex + 1) % (pages.length - 1)]
+    } else {
+      const bareNextPath = readNext.slice(1, -1)
 
-      return (
-        <div>
-          <h6
-            style={{
-              margin: 0,
-              fontSize: fontSizeToMS(-1).fontSize,
-              lineHeight: fontSizeToMS(-1).lineHeight,
-              letterSpacing: -0.5,
-            }}
-          >
-            READ THIS NEXT:
-          </h6>
-          <h3
-            style={{
-              marginBottom: rhythm(1/4),
-            }}
-          >
-            <Link
-              to={{
-                pathname: link(nextPost.path),
-                query: {
-                  readNext: true,
-                },
-              }}
-            >
-              {nextPost.data.title}
-            </Link>
-          </h3>
-          <p>{body}</p>
-          <hr />
-        </div>
+      nextPost = find(pages, (page) =>
+        includes(page.path, bareNextPath)
       )
     }
+    // Create pruned version of the body.
+    const html = nextPost.data.body
+    const body = prune(html.replace(/<[^>]*>/g, ''), 200)
+
+    return (
+      <div>
+        <h6
+          style={{
+            margin: 0,
+            fontSize: fontSizeToMS(-1).fontSize,
+            lineHeight: fontSizeToMS(-1).lineHeight,
+            letterSpacing: -0.5,
+          }}
+        >
+          READ THIS NEXT:
+        </h6>
+        <h3
+          style={{
+            marginBottom: rhythm(1/4),
+          }}
+        >
+          <Link
+            to={{
+              pathname: link(nextPost.path),
+              query: {
+                readNext: true,
+              },
+            }}
+          >
+            {nextPost.data.title}
+          </Link>
+        </h3>
+        <p>{body}</p>
+        <hr />
+      </div>
+    )
   }
 }
 
