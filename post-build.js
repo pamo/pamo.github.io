7245cc9e720711a65918b2e85650534e65622b92
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-03-18 10:19:43 -0700

    Introduces Imagemin to post build step to optimize jpgs

--- post-build.js
+++ post-build.js
@@ -1,17 +1,48 @@
+import async from 'async'
 import fs from 'fs-extra'
+import glob from 'glob'
+import Imagemin from 'imagemin'
 
-function copyRootAssets () {
-  const files = ['CNAME', 'README.md']
-  const path = 'public/'
+function minifyImages(pages, callback) {
+  const optimize = function optimizeImage(image, destination) {
+    return new Imagemin()
+      .src(image)
+      .dest(destination)
+      .use(Imagemin.jpegtran({
+        progressive: true,
+      }))
+      .run((error) => {
+        if (error) callback(error)
+      })
+  }
 
-  files.forEach((file) => {
-    fs.copy(file, `${path}${file}`, (error) => {
-      if (error) console.log(error)
-      console.log(`copying ${file} to ${path}`)
+  pages.forEach((page) => {
+    const directory = `${__dirname}/pages/${page.file.dirname}/`
+    const destination = `${__dirname}/public/${page.path}/`
+    const globString = `${directory}?(*.jpg|*.png|*.pdf|*.gif|*.ico|*.svg)`
+
+    glob(globString, null, (e, files) => {
+      async.map(files, (image) => {
+        optimize(image, destination)
+      }, (error, results) => callback(error, results))
     })
   })
 }
+
+function copyRootAssets() {
+  const files = ['CNAME', 'README.md']
+  const publicPath = 'public/'
+
+  files.forEach((file) => {
+    fs.copy(file, `${publicPath}${file}`, (error) => {
+      if (error) console.log(error)
+      console.log(`copying ${file} to ${publicPath}`)
+    })
+  })
+}
+
 module.exports = (pages, callback) => {
   copyRootAssets()
+  minifyImages(pages, callback)
   callback()
 }
