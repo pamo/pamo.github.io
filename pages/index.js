727d10f0bd600d4077c35ea2eb5a53f3a131c12d
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-04-07 15:35:44 +1000

    Adds twitter required tags for large summary cards

--- pages/index.js
+++ pages/index.js
@@ -44,12 +44,15 @@ class BlogIndex extends React.Component {
           meta={[
             { property: 'og:url', content: config.blogUrl },
             { property: 'og:type', content: 'blog' },
+            { property: 'og:title', content: config.blogTitle },
             { property: 'og:site_name', content: config.blogTitle },
             { property: 'og:image', content: fullImagePath },
             { property: 'fb:admins', content: config.fbAdminsId },
+            { name: 'twitter:title', content: config.blogTitle },
             { name: 'twitter:card', content: 'summary_large_image' },
             { name: 'twitter:site', content: config.authorTwitter },
             { name: 'twitter:creator', content: config.authorTwitter },
+            { name: 'twitter:description', content: `${config.authorTwitter} spends more time tweaking the CSS and markup of this blog than writing.`},
             { name: 'twitter:image', content: fullImagePath },
           ]}
           defaultTitle={ config.blogTitle }
