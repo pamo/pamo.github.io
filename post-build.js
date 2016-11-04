34e586e3192541ef644917fd0c42813f2b02f72f
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-11-03 21:33:22 -0700

    Updates imagemin, processes jpegs

--- post-build.js
+++ post-build.js
@@ -1,7 +1,10 @@
 import fs from 'fs-extra';
+import imagemin from 'imagemin';
+import mozJPEG from 'imagemin-mozjpeg';
+import path from 'path';
 
 function copyRootAssets() {
-  const files = ['CNAME', 'README.md', 'keybase.txt'];
+  const files = ['README.md', 'keybase.txt'];
   const publicPath = 'public/';
 
   files.forEach((file) => {
@@ -12,7 +15,17 @@ function copyRootAssets() {
   });
 }
 
+function optimizeImages(pages) {
+  const routes = pages.filter(p => p.path !== undefined);
+  routes.map(route => imagemin([`${path.join(__dirname, 'pages', route.file.dir)}/*.jpg`], path.join(__dirname, 'public', route.path), {
+    plugins: [mozJPEG({
+      quality: 85,
+    })],
+  }).then(files => console.log(`optimized ${files.length} images for ${route.path}`)));
+}
+
 module.exports = (pages, callback) => {
   copyRootAssets();
+  optimizeImages(pages);
   callback();
 };
