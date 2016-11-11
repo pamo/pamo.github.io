330876ff1eaba616f9ef4d6b77baa8995f8d68c3
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-11-11 12:20:42 -0800

    simplified quote styles and moved responsibitlity to react-typography

--- utils/typography.js
+++ utils/typography.js
@@ -1,13 +1,36 @@
 import Typography from 'typography';
+import { MOBILE_MEDIA_QUERY } from 'typography-breakpoint-constants';
+import gray from 'gray-percentage';
 import theme from 'typography-theme-fairy-gates';
 
-theme.overrideThemeStyles = ({ rhythm }) => ({
+theme.overrideThemeStyles = ({ rhythm, scale }) => ({
   a: {
     backgroundImage: 'none',
+  },
+  blockquote: {
+    ...scale(1),
+    fontFamily: 'athelas',
+    lineHeight: rhythm(1.5),
+    border: 0,
+    color: gray(5),
+    paddingLeft: 0,
+    marginLeft: 0,
+    marginRight: 0,
+  },
+  '.twitter-tweet': {
   },
   'h1,h2,h3,h4,h5,h6': {
     marginTop: rhythm(0.5),
     marginBottom: rhythm(0.5),
+  },
+  [MOBILE_MEDIA_QUERY]: {
+    blockquote: {
+      color: gray(5),
+      border: 0,
+      paddingLeft: rhythm(9/16),
+      marginLeft: rhythm(-3/4),
+      marginRight: 0,
+    },
   },
 });
 
